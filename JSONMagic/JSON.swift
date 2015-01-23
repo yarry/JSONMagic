aa9767075cc3b7b80dfe7516aaa0e0089f723cb7
Yaroslav Ponomarenko <yarryp@gmail.com>
2015-01-23 03:24:38 +0300

    rename init params for JSON

--- JSONMagic/JSON.swift
+++ JSONMagic/JSON.swift
@@ -16,26 +16,26 @@ public enum JSON {
     case JSONNumber(NSNumber)
     case JSONNull
     
-    public init?(data: NSData) {
+    public init?(jsonData: NSData) {
         var error:NSErrorPointer = nil
-        if let json: AnyObject = NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments, error: error) as AnyObject?  {
-            self = JSON(json:json)
+        if let json: AnyObject = NSJSONSerialization.JSONObjectWithData(jsonData, options: .AllowFragments, error: error) as AnyObject?  {
+            self = JSON(jsonObject:json)
         }
         else {
             return nil
         }
     }
     
-    public init(json: AnyObject) {
+    public init(jsonObject: AnyObject) {
         
-        switch json {
+        switch jsonObject {
         case let v as [AnyObject]:
-            self = .JSONArray(v.map{JSON(json:$0)})
+            self = .JSONArray(v.map{JSON(jsonObject:$0)})
         case let v as [String:AnyObject]:
             var object: [String:JSON] = [:]
             for key in v.keys {
                 if let value: AnyObject = v[key] {
-                    object[key] = JSON(json:value)
+                    object[key] = JSON(jsonObject:value)
                 } else {
                     object[key] = .JSONNull
                 }
